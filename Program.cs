59ab42ad5eeab5d397e2b4c7d433610d13cfd0a9
Erismar Oliveira <erismarpro@hotmail.com>
2024-01-05 18:45:57 -0300

    update .net 8

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using erismaroliveira;
+using ErismarOliveira;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
